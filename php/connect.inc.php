6fc6499e818a37065595ad8d610bb83e20fcc4b3
Turner Halligan <turnerhusa@gmail.com>
2017-01-19 17:39:11 -0500

    Added connection to users database

--- php/connect.inc.php
+++ php/connect.inc.php
@@ -3,5 +3,12 @@
 	define('DB_USERNAME', 'scouting');
 	define('DB_PASSWORD', 'Pi,=1676_Oneers2');
 	define('DB_DATABASE', 'scouting_2017');
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	$scouting = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+
+	define('DB_SERVER2', 'localhost');
+	define('DB_USERNAME2', 'pi_accounts');
+	define('DB_PASSWORD2', 'Pi,=1676_Oneers2');
+	define('DB_DATABASE2', 'pioneer_accounts');
+	$login = mysqli_connect(DB_SERVER2,DB_USERNAME2,DB_PASSWORD2,DB_DATABASE2);
 ?>
