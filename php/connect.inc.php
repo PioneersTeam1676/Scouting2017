4c2b6bd184bbb14d5d80ed4a6908fee56073ad57
Turner Halligan <turnerhusa@gmail.com>
2017-01-19 17:08:38 -0500

    Fixed connect.inc FINAL
    Final fix to connect.inc

--- php/connect.inc.php
+++ php/connect.inc.php
@@ -2,6 +2,6 @@
 	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'scouting');
 	define('DB_PASSWORD', 'Pi,=1676_Oneers2');
-	define('DB_DATABASE', 'Scouting');
+	define('DB_DATABASE', 'scouting_2017');
 	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
