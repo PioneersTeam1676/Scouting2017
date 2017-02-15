135df5b8a3df27695a935505fdb0ab887a1aaa84
Turner Halligan <turnerhusa@gmail.com>
2017-02-15 18:38:15 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
-console.log("Loaded main.js");
 
-var pages = 
-   [document.getElementById("div_loginPage"),
-    document.getElementById("div_scoutingPage"),
-    document.getElementById("div_usersPage"),
-    document.getElementById("div_rawDataPage"),
-    document.getElementById("div_teamsPage"),
-    document.getElementById("div_contactPage")];
+    console.log("Loaded main.js");
 
-//Pre: The page number that the user is changing to
-//Post: Changes to the page selected
-function changePage(pageNum){
-    console.log("Changing to page : " + pageNum);
-    for (var i = 0; i < pages.length; i++){
-        pages[i].style.visibility = "hidden";
-        pages[i].style.display = "none";
+    $( document ).ready(function() {
+        console.log( "ready!" );
+        $('textarea').autoResize();
+    });
+        
+
+    var pages = 
+       [document.getElementById("div_loginPage"),
+        document.getElementById("div_scoutingPage"),
+        document.getElementById("div_usersPage"),
+        document.getElementById("div_rawDataPage"),
+        document.getElementById("div_teamsPage"),
+        document.getElementById("div_contactPage")];
+
+    //Pre: The page number that the user is changing to
+    //Post: Changes to the page selected
+    function changePage(pageNum){
+        console.log("Changing to page : " + pageNum);
+        for (var i = 0; i < pages.length; i++){
+            pages[i].style.visibility = "hidden";
+            pages[i].style.display = "none";
+        }
+        
+        pages[pageNum].style.visibility = "visible";
+        var pageID = "#"+pages[pageNum].id;
+        $(pageID).fadeIn("slow");
+        pages[pageNum].style.display = "block";
     }
-    
-    pages[pageNum].style.visibility = "visible";
-    pages[pageNum].style.display = "block";
-}
 
-
-// ON BOOT METHODS :
-
-changePage(0);
+        changePage(0);
